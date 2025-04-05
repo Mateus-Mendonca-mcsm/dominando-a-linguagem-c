af2b44734581e49bf9793ff609476e1ca822f52d
Mateus Mendonça <mateusmcsm22@gmail.com>
2025-04-05 10:06:27 -0300

    Aula pratica

---
+++ meu_primeiro_programa.c
@@ -0,0 +1,17 @@
+# include <stdio.h> // Biblioteca padrão de entrada e saída de dados
+
+int main() {
+    printf("olá, mundo!\n");
+    return 0; // Retorna 0 para indicar que o programa terminou com sucesso
+}
+
+
+
+//COMENTÁRIOS ÚTEIS
+/*1- 👉 os parenteses depois de main, indicam que main é um bloco de construção de programa chamado de função.
+    A função main é o ponto de entrada do programa, ou seja, é onde o programa começa a ser executado.
+    as chaves { e } delimitam o início e o fim do bloco de código da função main.
+2- 👉 o ponto e vírgula no final de cada linha indica o fim de uma instrução.
+3- 👉 o printf é uma função que imprime na tela o que está entre aspas. O \n indica uma quebra de linha.
+4- 👉 o return 0 indica que o programa terminou com sucesso. O 0 é um código de saída padrão que indica que não houve erros.
+*/
